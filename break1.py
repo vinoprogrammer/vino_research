36a892b5d398a6416a08e9f80b43a72421a9abaf
vinoprogrammer <144443837+vinoprogrammer@users.noreply.github.com>
2023-09-11 10:18:02 +0530

    Add files via upload

---
+++ break1.py
@@ -0,0 +1,25 @@
+s=input()
+lower_s=s.lower()
+for i in lower_s:
+    if i=='o' or i=='a' or i=='e' or i=='i' or i=='u':
+        break
+    else:
+        print(i)
+
+        
+
+    
+
+
+    
+
+
+    
+
+
+
+    
+    
+
+    
+
